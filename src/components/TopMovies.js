52f5fd2bd255c2c0c387abaafa45f16543a63df1
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-07-02 13:05:40 +0200

    add header and list container to TopMovies component

--- src/components/TopMovies.js
+++ src/components/TopMovies.js
@@ -4,7 +4,15 @@ class TopMovies extends React.Component {
 
     render() {
         return(
-            <div>TopMovies</div>
+            <div className="ui container">
+                <h2 className="ui center aligned icon header">
+                    <i className="circular video icon"></i>
+                        Top Rated Movies
+                </h2>
+                <div className="movies-list">
+
+                </div>
+            </div>
         );
     };
 }
