5421a95ab24ba5febaf8fbcac00ecac391f21e5b
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-07-02 16:27:31 +0200

    add tmdb logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,6 +6,9 @@ const NavBar = () => {
         <div className="ui inverted vertical segment">
             <div className="ui inverted secondary pointing menu">
                 <Link to="/" className="active item">
+                    <img src="https://www.themoviedb.org/assets/2/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg" alt="tmdb-logo"/>
+                </Link>
+                <Link to="/" className="active item">
                     Home
                 </Link>
                 <Link to="/top-movies" className="item">
