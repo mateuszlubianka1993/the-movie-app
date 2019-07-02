bd6f8761694ec9c8ec116460ee48709015c17a7e
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-07-02 11:03:48 +0200

    add Header.js component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from './Header';
 
 class App extends React.Component {
     
     render() {
         return(
-            <div>APP</div>
+            <div className="app-container">
+                <Header />
+            </div>
         );
     };
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header-container container">
+            <h1 className="header-txt">The Movie App</h1>
+        </div>
+    );
+};
+
+export default Header;
