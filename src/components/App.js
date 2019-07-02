c483bb6e09e940b0318fcf6f021ab0edd9ffaded
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-07-02 11:20:59 +0200

    install react-router-dom and add NavBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import NavBar from './NavBar';
 
 class App extends React.Component {
     
@@ -7,6 +8,7 @@ class App extends React.Component {
         return(
             <div className="app-container">
                 <Header />
+                <NavBar />
             </div>
         );
     };
---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const NavBar = () => {
+    return(
+        <div className="ui inverted segment">
+            <div className="ui inverted secondary pointing menu">
+                <a className="active item">
+                    Home
+                </a>
+                <a className="item">
+                    Top Movies
+                </a>
+                <a className="item">
+                    Top TV Shows
+                </a>
+                <a className="item">
+                    Top TV Shows
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
