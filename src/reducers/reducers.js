55b3e7623f88cc20a116f5d920fd67c6f891b64c
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-07-02 15:49:08 +0200

    add nowPlayingReducer

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import topMoviesReducer from './topMoviesReducer';
 import topShowsReducer from './topShowsReducer';
+import nowPlayingReducer from './nowPlayingReducer';
 
 export default combineReducers({
     topMovies: topMoviesReducer,
-    topShows: topShowsReducer
+    topShows: topShowsReducer,
+    nowPlaying: nowPlayingReducer
 });
