82df5a5737f787e72f3e9b22b9877fda3123b280
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-07-03 16:41:53 +0200

    add inputValueReducer

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -3,10 +3,12 @@ import topMoviesReducer from './topMoviesReducer';
 import topShowsReducer from './topShowsReducer';
 import nowPlayingReducer from './nowPlayingReducer';
 import popularPeopleReducer from './popularPeopleReducer';
+import inputValueReducer from './inputValueReducer';
 
 export default combineReducers({
     topMovies: topMoviesReducer,
     topShows: topShowsReducer,
     nowPlaying: nowPlayingReducer,
-    popularPeople: popularPeopleReducer
+    popularPeople: popularPeopleReducer,
+    inputValue: inputValueReducer
 });
