9cd10f5e3d9b181dfbda566b49dd205e8f69852d
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-07-03 13:53:48 +0200

    add known for property to person component

--- src/components/Person.js
+++ src/components/Person.js
@@ -3,27 +3,26 @@ import React from 'react';
 class Show extends React.Component {
     render() {
         const {person} = this.props;
+        const knownForRender = person.known_for.map(x=>{
+            return(
+                <div className="item">
+                        <img src={`https://image.tmdb.org/t/p/w45/${x.poster_path}`} alt="poster"/>
+                        <div className="header">{x.title}</div>
+                </div>
+            );
+        })
         return(
             <div className="ui card">
                 <div className="image">
-                    <img src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${person.profile_path}`} alt="avatar" />
+                    <img src={`https://image.tmdb.org/t/p/w200/${person.profile_path}`} alt="avatar" />
                 </div>
                 <div className="content">
                     <h5 className="header">{person.name}</h5>
-                    <div className="meta">
-                        {/* <span className="date">{person.release_date}</span> */}
-                    </div>
                     <div className="description">
-                        {/* {person.overview} */}
-                    </div>
-                </div>
-                <div className="extra content">
-                    <div>
-                        <i className="thumbs up icon"></i>
-                            {/* {person.vote_average}/10 */}
-                    </div>
-                    <div>
-                            {/* Votes: {person.vote_count} */}
+                        <h5>Known for:</h5>
+                        <div className="">
+                            {knownForRender}
+                        </div>
                     </div>
                 </div>
             </div>
