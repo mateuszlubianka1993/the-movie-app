534743c90ec9fa8ca9abd892c89b663a8f65aa39
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-07-03 12:57:48 +0200

    add popularPeopleReducer

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import topMoviesReducer from './topMoviesReducer';
 import topShowsReducer from './topShowsReducer';
 import nowPlayingReducer from './nowPlayingReducer';
+import popularPeopleReducer from './popularPeopleReducer';
 
 export default combineReducers({
     topMovies: topMoviesReducer,
     topShows: topShowsReducer,
-    nowPlaying: nowPlayingReducer
+    nowPlaying: nowPlayingReducer,
+    popularPeople: popularPeopleReducer
 });
