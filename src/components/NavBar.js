1cc4e8ebad34722839f2c4c2824ac2370ee96035
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-07-04 09:51:00 +0200

    add NavLinks instead Links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 const NavBar = () => {
     return(
         <div className="ui inverted vertical segment">
             <div className="ui inverted secondary stackable container pointing menu">
-                <Link to="/" className="active item">
+                <NavLink to="/" className="item">
                     <img src="https://www.themoviedb.org/assets/2/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg" alt="tmdb-logo"/>
-                </Link>
-                <Link to="/" className="active item">
+                </NavLink>
+                <NavLink to="/" className="item">
                     Home
-                </Link>
-                <Link to="/top-movies" className="item">
+                </NavLink>
+                <NavLink to="/top-movies" className="item">
                     Top Movies
-                </Link>
-                <Link to="/top-shows" className="item">
+                </NavLink>
+                <NavLink to="/top-shows" className="item">
                     Top TV Shows
-                </Link>
-                <Link to="/now-playing" className="item">
+                </NavLink>
+                <NavLink to="/now-playing" className="item">
                     Now Playing
-                </Link>
-                <Link to="/search" className="item">
+                </NavLink>
+                <NavLink to="/search" className="item">
                     Search
-                </Link>
+                </NavLink>
             </div>
         </div>
     );
