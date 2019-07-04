253599e48e112ad672889dd6483f6ed361cca6bd
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-07-04 11:05:02 +0200

    Updates

---
+++ precache-manifest.74c023bd8280a86e7aca0f6e4e86cb10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17821b83e806d0f5b7a3ba9d885da510",
+    "url": "/the-movie-app/index.html"
+  },
+  {
+    "revision": "14fc19d4c873f924c28b",
+    "url": "/the-movie-app/static/css/main.189aef8d.chunk.css"
+  },
+  {
+    "revision": "9dee67ae795ce593bcd5",
+    "url": "/the-movie-app/static/js/2.1e74ee55.chunk.js"
+  },
+  {
+    "revision": "14fc19d4c873f924c28b",
+    "url": "/the-movie-app/static/js/main.da53c532.chunk.js"
+  },
+  {
+    "revision": "9c8788efce2a31146728",
+    "url": "/the-movie-app/static/js/runtime~main.0523cf36.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/the-movie-app/precache-manifest.74c023bd8280a86e7aca0f6e4e86cb10.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/the-movie-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
